a2d299688bb47b6c5dd1f8e5b164e40b8d3ae53b
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-08-22 23:41:16 -0300

    Created entity Autor

---
+++ src/main/java/br/libdolf/backendlivraria/entities/Autor.java
@@ -0,0 +1,44 @@
+package br.libdolf.backendlivraria.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String email;
+
+    @DateTimeFormat(pattern = "dd-mm-yyy")
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    private String cpf;
+
+    @ManyToMany(mappedBy = "autor")
+    private Set<Obra> obras = new HashSet<>();
+
+    public enum Gender{
+        MALE, FEMALE, OTHER
+    }
+
+}
