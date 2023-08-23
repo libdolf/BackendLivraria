715594cdc2679eeb5294f64869d2e19f1e1b378d
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-08-22 23:30:38 -0300

    First Commit

---
+++ src/main/java/br/libdolf/backendlivraria/BackEndLivrariaApplication.java
@@ -0,0 +1,13 @@
+package br.libdolf.backendlivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndLivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/libdolf/backendlivraria/BackEndLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.libdolf.backendlivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
